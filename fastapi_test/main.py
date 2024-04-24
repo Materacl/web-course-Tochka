69a4519cd5f1a18f1c37f03f1733b4088bc91086
Materacl <yurchenko.stas15@gmail.com>
2024-04-24 20:37:18 +0500

    Finished fastapi_test

--- fastapi_test/main.py
+++ fastapi_test/main.py
@@ -1,20 +1,26 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 
 app = FastAPI()
 
+
 @app.get("/echo")
-async def read_headers(request: Request):
+async def echo_headers(request: Request):
     headers = dict(request.headers)
     return headers
 
+
 @app.get("/hello")
 async def hello():
-    return "hello"
+    return {"message": "hello"}
+
 
 @app.post("/echo")
-async def echo_post(request_body: str):
-    return request_body
+async def echo_body(request: Request):
+    body = await request.json()
+    return body
+
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000,)
+
+    uvicorn.run(app, host="127.0.0.1", port=8000, )
