634ec6c1b6173a98a23f0457f1a6e5d16cfe6212
Materacl <yurchenko.stas15@gmail.com>
2024-06-17 15:16:50 +0500

    Payment fixes

--- booking_service/api/v1/routers/payments.py
+++ booking_service/api/v1/routers/payments.py
@@ -11,8 +11,9 @@ from ..database import get_db
 from ..schemas import PaymentCreate, Payment
 from ..utils.auth import get_current_active_user
 from ..schemas import User
-from ..crud.payments import create_payment, update_payment_status
+from ..crud.payments import create_payment, update_payment_status, get_payment
 from ..crud.bookings import get_booking
+from ..crud.sessions import get_session
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -28,13 +29,13 @@ router = APIRouter(
 )
 
 
-@router.post("/create-payment-intent", response_model=JSONResponse, status_code=status.HTTP_201_CREATED,
+@router.post("/create-payment-intent", response_model=dict, status_code=status.HTTP_201_CREATED,
              summary="Create a Stripe Payment Intent")
 async def create_payment_intent(
         payment: PaymentCreate,
         db: Session = Depends(get_db),
         current_user: User = Depends(get_current_active_user)
-) -> JSONResponse:
+) -> dict:
     """
     Create a Stripe Payment Intent.
 
@@ -49,20 +50,27 @@ async def create_payment_intent(
     try:
         db_booking = get_booking(db, payment.booking_id)
         if db_booking.payment_id:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                                detail="Payment with this booking_id already exist.")
+            current_payment = get_payment(db, db_booking.payment_id)
+            if current_payment.status != PaymentStatus.FAILED:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                    detail="Payment with this booking_id already exist.")
+        db_session = get_session(db, db_booking.session_id)
+        if db_session:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="Session for this payment is not found.")
+        amount = int(db_session.price * len(db_booking.reservations) * 100)
         # Create a PaymentIntent with the order amount and currency
         intent = stripe.PaymentIntent.create(
-            amount=int(payment.amount * 100),  # Stripe expects the amount in cents
+            amount=amount,  # Stripe expects the amount in cents
             currency="usd",
             metadata={"integration_check": "accept_a_payment"},
         )
 
         # Save the Payment information in the database
-        db_payment = create_payment(db, payment)
+        db_payment = create_payment(db, payment, )
         logger.info(f"PaymentIntent created: {intent['id']} for Booking ID: {payment.booking_id}")
 
-        return JSONResponse({"client_secret": intent['client_secret']})
+        return {"client_secret": intent['client_secret']}
     except Exception as e:
         logger.error(f"Error creating PaymentIntent: {e}")
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error creating PaymentIntent")
@@ -110,4 +118,4 @@ async def stripe_webhook(request: Request, db: Session = Depends(get_db)):
             else:
                 logger.error(f"Payment not found for payment ID: {payment_id}")
 
-    return JSONResponse({"status": "success"})
+    return {"status": "success"}
